31c089f1e71751a675856e49409662a895ef63b6
[ianizaguirre] <ian@izaguir.re>
2018-04-11 15:26:45 -0400

    Update: Added styles to List area

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+  list-style: none;
+  border-color: #e1e1e1;
+  border-top-style: solid;
+  border-bottom-style: solid;
+  border-width: 1px;
+`;
+const FlexContainer = styled.div`
+  display: flex;
+`;
+const Column = styled.div`
+  width: 33.3%;
+`;
 class Task extends Component {
   render() {
     const { name } = this.props.iterateDetails;
 
     return (
-      <li>
-        <p>{name}</p>
+      <ListItem>
+        <FlexContainer>
+          <Column>
+            <p>{name}</p>
+          </Column>
 
-        <button> Remove Task </button>
-      </li>
+          <Column>
+            <button> Remove Task </button>
+          </Column>
+        </FlexContainer>
+      </ListItem>
     );
   }
 }
