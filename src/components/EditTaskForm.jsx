e194f6f37666b9bc5185b83bbe607f4c4b72bf45
[ianizaguirre] <ian@izaguir.re>
2018-05-02 03:00:32 -0400

    Updated: Css in EditTaskForm

--- src/components/EditTaskForm.jsx
+++ src/components/EditTaskForm.jsx
@@ -1,27 +1,65 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 import styled from 'styled-components';
 
-const ListItem = styled.li`
+const ListItemsWrapper = styled.li`
   list-style: none;
   border-color: #e1e1e1;
   border-top-style: solid;
   border-bottom-style: solid;
   border-width: 1px;
+  padding-top: 11px;
+  padding-bottom: 11px;
   margin-bottom: 15px;
 `;
-const FlexContainer = styled.div`
+const FlexContainerWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const FlexContainerColumn = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-direction: column;
+`;
+const FlexContainerRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-direction: row;
 `;
 const Column1 = styled.div`
-  align-self: center;
   margin-right: auto;
-`;
-const Column2 = styled.div`
-  align-self: center;
-  margin-left: auto;
+  flex: 1 0 50%;
 `;
 
+const Column2 = Column1.extend`
+  font-size: 13px;
+  color: #535a5b;
+  flex: none;
+
+  &:after {
+    content: '';
+    position: absolute;
+    border-bottom: solid 1px #535a5b;
+    width: 40px;
+    padding-top: 5px;
+    height: auto;
+  }
+`;
+
+const Column3 = Column1.extend`
+  flex: 0 1 100%;
+  margin-top: 4px;
+`;
+
+const Input = styled.input`
+  font-size: 14px;
+  color: #535a5b;
+`;
 // const Button = styled.button`
 //   color: #e44232;
 //   background-color: #ffffff;
@@ -54,40 +92,44 @@ const Column2 = styled.div`
 // }
 const Button = styled.button`
   /* Adapt the colours based on primary prop */
-  background: red;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  visibility: ${props => (props.buttonIsVisible ? 'visible' : 'hidden')};
+  color: ${props => (props.cancel ? '#7d8485' : '#ffffff')};
+  background-color: ${props => (props.cancel ? '#ffffff' : '#359010')};
+  font-size: 13px;
+  line-height: 20px;
+  font-weight: ${props => (props.cancel ? '400' : '500')};
+  border: ${props => (props.cancel ? '2px solid transparent' : '2px solid')};
+  border-radius: 4px;
+  cursor: pointer;
+
+  margin-right: 0.5em;
+  padding: 0.25em 0.7em;
+
+  display: ${props => (props.buttonIsVisible ? 'inline-block' : 'none')};
 `;
+//visibility: ${props => (props.buttonIsVisible ? 'visible' : 'hidden')};
+
+// ================================================
+// console.log(event.currentTarget); // ==> returns => whole <input... tag
+// =======================
+// console.log(event.currentTarget.name); // ==> returns => name (1word)
+// =======================
+// console.log(event.currentTarget.value); // ==> returns => whole input value
+// =======================
+// console.log(this.props.index); // ==> returns => task1524783116410
+// =============================
+// console.log(this.props.taskKeysValue); // ==> returns =>
+// {created: "Apr 26", name: "eeeeedfssdsss"}
+// =======================
+// console.log(this.props.taskKeysValue.name); // ==> returns =>
+// whole input value
+// ================================================
 
 class EditTaskForm extends Component {
   handleChange = event => {
     event.preventDefault();
     console.log('Change in Input Detected');
-
-    // =======================
-    // console.log(event.currentTarget); // ==> returns => whole <input... tag
-    // =======================
-    // console.log(event.currentTarget.name); // ==> returns => name (1word)
-    // =======================
-    // console.log(event.currentTarget.value); // ==> returns => whole input value
-    // =======================
-    // console.log(this.props.index); // ==> returns => task1524783116410
-    // =============================
-    // console.log(this.props.taskKeysValue); // ==> returns =>
-    // {created: "Apr 26", name: "eeeeedfssdsss"}
-    // =======================
-    // console.log(this.props.taskKeysValue.name); // ==> returns =>
-    // whole input value
-    // ================================================
-    // Note: const updatedThisTask ={} is an object, not a function ?
     // update that Task
     // 1. Take a copy of the current task
-    // console.log('===========3========>');
-    // console.log(this.props.originalTaskValue);
     const updatedThisTask = {
       ...this.props.taskKeysValue, //{created: "Apr 26", name: "eeeeedfssdsss"}
       [event.currentTarget.name]: event.currentTarget.value
@@ -124,40 +166,43 @@ class EditTaskForm extends Component {
 
     // Send back upstream
     this.props.updateTask(this.props.index, this.props.sendRevertedTask);
-    console.log('===========xoxoxo========>');
+    console.log('Clicked Cancel ===========xoxoxo========>');
     // console.log(revertThisTask);
   };
 
   render() {
     return (
-      <Fragment>
-        <ListItem>
-          <FlexContainer>
-            <Column1>
-              <form>
-                <input
+      <form>
+        <ListItemsWrapper>
+          <FlexContainerWrapper>
+            <FlexContainerColumn>
+              <Column1>
+                <Input
                   type="text"
                   name="name"
                   onChange={this.handleChange}
                   onFocus={this.handleFocus}
                   value={this.props.taskKeysValue.name}
                 />
-
+              </Column1>
+              <Column3>
                 <Button buttonIsVisible={this.props.isAvailable} onClick={this.handleClickSave}>
                   Save
                 </Button>
-                <Button buttonIsVisible={this.props.isAvailable} onClick={this.handleClickCancel}>
+                <Button cancel buttonIsVisible={this.props.isAvailable} onClick={this.handleClickCancel}>
                   Cancel
                 </Button>
-              </form>
-            </Column1>
-            <Column2>
-              <p> Date </p>
-              <p> {this.props.taskKeysValue.created} </p>
-            </Column2>
-          </FlexContainer>
-        </ListItem>
-      </Fragment>
+              </Column3>
+            </FlexContainerColumn>
+
+            <FlexContainerRow>
+              <Column2>
+                <p> {this.props.taskKeysValue.created} </p>
+              </Column2>
+            </FlexContainerRow>
+          </FlexContainerWrapper>
+        </ListItemsWrapper>
+      </form>
     );
   }
 }
