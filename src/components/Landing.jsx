94dc9be698d5adf71e954da25100395617cd90ba
[ianizaguirre] <ian@izaguir.re>
2018-06-07 03:53:45 -0400

    Add: Media queries

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,13 +15,13 @@ const Column = styled.div`
   min-height: 100vh;
   margin: 0 auto;
 
-  /* @media (max-width: 1200px) {
+  @media (max-width: 1200px) {
     width: ${props => (props.primary ? '50%' : '15%')};
   }
 
   @media (max-width: 750px) {
     width: ${props => (props.primary ? '100%' : '0')};
-  } */
+  }
 `;
 const MiddleColumn = Column.extend`
   border-left: 1px solid #f0f0f0;
