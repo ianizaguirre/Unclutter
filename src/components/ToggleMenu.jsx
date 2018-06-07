9c1766242585b662803536b0b2a27f3e80a7ed49
[ianizaguirre] <ian@izaguir.re>
2018-06-07 07:07:21 -0400

    Update: ToggleMenu MenuContents component

--- src/components/ToggleMenu.jsx
+++ src/components/ToggleMenu.jsx
@@ -33,7 +33,7 @@ const MenuContents = styled.div`
     /* margin-left: auto;
     margin-top: auto;
     position: relative; */
-    margin-top: ${props => (props.toolBoxOpen ? '-87px' : '-50px')};
+    margin-top: ${props => (props.toolBoxOpen ? '-88px' : '-51px')};
     margin-left: -17px;
     /* top: 218px; */
     width: 85px;
